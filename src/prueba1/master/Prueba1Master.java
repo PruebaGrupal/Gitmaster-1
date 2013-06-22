1699dde5db958197a078c01a1bbc3412042783f4
Villarreal <Villarreal@Villarreal-PC>
2013-06-22 15:28:56 -0600

    cambios a MI repositorio

---
+++ src/prueba1/master/Prueba1Master.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1.master;
+
+/**
+ *
+ * @author Villarreal
+ */
+public class Prueba1Master {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      
+        
+    
+    }
+}
